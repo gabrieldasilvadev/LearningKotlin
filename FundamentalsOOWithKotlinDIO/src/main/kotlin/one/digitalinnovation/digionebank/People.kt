5541fe5bda2ddc27f9d6886e357968440a1cace6
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-05-01 02:13:13 -0300

    Understand inner class

--- FundamentalsOOWithKotlinDIO/src/main/kotlin/one/digitalinnovation/digionebank/People.kt
+++ FundamentalsOOWithKotlinDIO/src/main/kotlin/one/digitalinnovation/digionebank/People.kt
@@ -3,6 +3,10 @@ package one.digitalinnovation.digionebank
 class People {
     var name: String = "Gabriel"
     var cpf: String = "123.456.789.11"
+
+    inner class Adress {
+        var street: String = "Rua teste"
+    }
 }
 
 fun main() {
@@ -10,4 +14,6 @@ fun main() {
 
     println(gabriel.name)
     println(gabriel.cpf)
+
+    println(gabriel.Adress().street)
 }
