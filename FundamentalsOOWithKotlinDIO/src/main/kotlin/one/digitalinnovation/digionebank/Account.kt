da119a487bb4f6da1e17cdca9cd654363b6111cb
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-05-01 04:52:07 -0300

    Created account class

---
+++ FundamentalsOOWithKotlinDIO/src/main/kotlin/one/digitalinnovation/digionebank/Account.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Account(
+  val agency: String,
+  val number: String,
+  val balance: BigDecimal
+) {
+  fun deposit(value: BigDecimal) {
+
+  }
+
+  fun withdrawal(value: BigDecimal){
+
+  }
+}
