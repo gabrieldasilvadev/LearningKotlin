76c8655eca08b32b7bf0577b2a66babbc5faf554
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-05-01 02:25:54 -0300

    Add private set on cpf fot don't change your value

--- FundamentalsOOWithKotlinDIO/src/main/kotlin/one/digitalinnovation/digionebank/People.kt
+++ FundamentalsOOWithKotlinDIO/src/main/kotlin/one/digitalinnovation/digionebank/People.kt
@@ -3,10 +3,7 @@ package one.digitalinnovation.digionebank
 class People {
     var name: String = "Gabriel"
     var cpf: String = "123.456.789.11"
-
-    inner class Adress {
-        var street: String = "Rua teste"
-    }
+    private set
 }
 
 fun main() {
@@ -14,6 +11,4 @@ fun main() {
 
     println(gabriel.name)
     println(gabriel.cpf)
-
-    println(gabriel.Adress().street)
 }
