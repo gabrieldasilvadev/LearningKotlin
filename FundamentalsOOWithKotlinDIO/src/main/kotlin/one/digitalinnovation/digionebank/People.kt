95e4c6aad0c9b3e939580d1752603b0b6f5385e7
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-05-01 01:59:13 -0300

    Create Class People

---
+++ FundamentalsOOWithKotlinDIO/src/main/kotlin/one/digitalinnovation/digionebank/People.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class People {
+    var name: String = "Gabriel"
+    var cpf: String = "123.456.789.11"
+}
+
+fun main() {
+    val gabriel = People()
+
+    println(gabriel.name)
+    println(gabriel.cpf)
+}
