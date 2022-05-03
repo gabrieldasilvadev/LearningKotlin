227344ef8f8be29b49ba26fd7600aac4b8ea99b4
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-05-03 00:14:40 -0300

    Updated test employee manager

--- FundamentalsOOWithKotlinDIO/src/main/kotlin/br/com/gabrielkt/digiebank/test/TestEmployeeManager.kt
+++ FundamentalsOOWithKotlinDIO/src/main/kotlin/br/com/gabrielkt/digiebank/test/TestEmployeeManager.kt
@@ -3,6 +3,8 @@ package br.com.gabrielkt.digiebank.test
 import br.com.gabrielkt.digiebank.Manager
 
 fun main() {
-  val brandao = Manager(name = "Brandao", cpf = "435.217.326.12", salary = 15830.32)
+  val brandao = Manager(name = "Brandao", cpf = "435.217.326.12", salary = 15830.32, password = "senha123")
   PrintReportEmployee.printReport(brandao)
+
+  TestAuthentication().authenticate(brandao)
 }
