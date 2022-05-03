7b77d71d329eb705482b81b686d88fe6a8aef56f
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-05-03 00:14:27 -0300

    Updated Manager class

--- FundamentalsOOWithKotlinDIO/src/main/kotlin/br/com/gabrielkt/digiebank/Manager.kt
+++ FundamentalsOOWithKotlinDIO/src/main/kotlin/br/com/gabrielkt/digiebank/Manager.kt
@@ -3,7 +3,9 @@ package br.com.gabrielkt.digiebank
 class Manager(
   name: String,
   cpf: String,
-  salary: Double
-) : Employee(name = name, cpf = cpf, salary = salary) {
+  salary: Double,
+  val password: String
+) : Employee(name = name, cpf = cpf, salary = salary), Logon {
     override fun calculateBonus(): Double = salary * 0.4
+  override fun login(): Boolean = "senha123" == password
 }
