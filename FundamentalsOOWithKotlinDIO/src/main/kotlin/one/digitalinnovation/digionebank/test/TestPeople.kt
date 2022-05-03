1f5193c0643347b1bf6f0441daceb82f0dbcd64f
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-05-02 22:42:27 -0300

    Refacted TestPeople

---
+++ FundamentalsOOWithKotlinDIO/src/main/kotlin/one/digitalinnovation/digionebank/test/TestPeople.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.digionebank.test
+
+import one.digitalinnovation.digionebank.People
+
+fun main() {
+  val gabriel = People(name = "Gabriel", cpf = "111.222.333.44")
+
+  println(gabriel.name)
+  println(gabriel.cpf)
+}
